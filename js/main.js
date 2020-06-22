d3fedcdbae8dde77e798f35756c0441cb9e1cd63
claire_t <e.claire.thompson@gmail.com>
2020-06-22 17:40:18 -0400

    Nav menu tweaks

--- js/main.js
+++ js/main.js
@@ -7,9 +7,11 @@ navMenu.style.display = 'none';
 
 const toggleNav = () => {
     if (navMenu.style.display == 'none') {
-        navMenu.style.display = 'flex';
+        navMenu.style.display = 'block';
+        menuButton.className = 'fas fa-times';
     } else {
         navMenu.style.display = 'none';
+        menuButton.className = 'fas fa-bars';
     }
 };
 
