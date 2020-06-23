d64782ce1bd7290c45629d6a467d476c2483a123
claire_t <e.claire.thompson@gmail.com>
2020-06-23 09:38:51 -0400

    Menu toggle animation

--- js/main.js
+++ js/main.js
@@ -3,14 +3,14 @@
 const menuButton = document.getElementById('menu-button');
 const navMenu = document.getElementById('nav');
 
-navMenu.style.display = 'none';
+navMenu.style.height = '0px';
 
 const toggleNav = () => {
-    if (navMenu.style.display == 'none') {
-        navMenu.style.display = 'block';
+    if (navMenu.style.height == '0px') {
+        navMenu.style.height = '285px';
         menuButton.className = 'fas fa-times';
     } else {
-        navMenu.style.display = 'none';
+        navMenu.style.height = '0px';
         menuButton.className = 'fas fa-bars';
     }
 };
