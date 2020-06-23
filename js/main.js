55153644a8b8596c78d90a56866bfde4ef21a212
claire_t <e.claire.thompson@gmail.com>
2020-06-23 11:05:07 -0400

    Tablet styles

--- js/main.js
+++ js/main.js
@@ -3,15 +3,14 @@
 const menuButton = document.getElementById('menu-button');
 const navMenu = document.getElementById('nav');
 
-navMenu.style.height = '0px';
-
 const toggleNav = () => {
-    if (navMenu.style.height == '0px') {
-        navMenu.style.height = '285px';
-        menuButton.className = 'fas fa-times';
-    } else {
+
+    if (navMenu.style.height == '285px') {
         navMenu.style.height = '0px';
         menuButton.className = 'fas fa-bars';
+    } else {
+        navMenu.style.height = '285px';
+        menuButton.className = 'fas fa-times';
     }
 };
 
