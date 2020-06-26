47f1a4c739200c1d6ba42ae92d38896bc31b85ce
claire_t <e.claire.thompson@gmail.com>
2020-06-26 17:23:01 -0400

    Bug fix to nav menu on screen resize

--- js/main.js
+++ js/main.js
@@ -2,16 +2,35 @@
 
 const menuButton = document.getElementById('menu-button');
 const navMenu = document.getElementById('nav');
+const navItems = document.getElementById('nav-items');
 
 const toggleNav = () => {
-
-    if (navMenu.style.height == '285px') {
+    if ((navMenu.style.height == '285px') || (navMenu.style.height === 'unset')) {
         navMenu.style.height = '0px';
+        navItems.style.height = '0px';
         menuButton.className = 'fas fa-bars';
-    } else {
+    } else if (navMenu.style.height = '0px') {
         navMenu.style.height = '285px';
+        navItems.style.height = '260px';
+        navItems.style.transition = 'all 0.3s ease-out';
+        navMenu.style.transition = 'all 0.3s ease-out';
         menuButton.className = 'fas fa-times';
     }
 };
 
-menuButton.addEventListener('click', toggleNav)
+const navSize = () => {
+    if (screen.availWidth < 768) {
+        navMenu.style.height = '0px';
+        navItems.style.height = '0px';
+        menuButton.className = 'fas fa-bars';
+        navMenu.style.transition = 'none';
+    } else if (screen.availWidth >= 768) {
+        navMenu.style.height = 'unset';
+        navItems.style.height = 'unset'
+        navMenu.style.transition = 'none';
+        menuButton.className = 'fas fa-times';
+    }
+}
+
+window.addEventListener('resize', navSize);
+menuButton.addEventListener('click', toggleNav);
